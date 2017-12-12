b64a235bf6f64f6e2891a3b21ac5c02292902912
Michael Käufl <michael-k@users.noreply.github.com>
2017-12-12 16:35:07 +0100

    Drops support for Python 3.3
    Reached its EOL in September 2017
    https://www.python.org/dev/peps/pep-0398/#x-end-of-life

--- setup.py
+++ setup.py
@@ -174,8 +174,9 @@ setup_kwargs = dict(
         'Topic :: Software Development',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 )
 
